42f88d97f76fdc4d4239ed413adaa431fffc7f06
Hugues Touyon <hugues.touyon@gmail.com>
2022-11-21 15:27:38 +0100

    v0.3

--- connexion.php
+++ connexion.php
@@ -18,7 +18,7 @@ if (!empty($_POST)) {
             $query->bindValue(":email", $_POST["email"], PDO::PARAM_STR);
             $query->execute();
             $user = $query->fetch();
-        
+
             if (!$user) {
                 $_SESSION["error"][] = "Utilisateur ou mot de passe incorrect";
             } elseif (!password_verify($_POST["pass"], $user["pass"])) {
@@ -36,14 +36,22 @@ if (!empty($_POST)) {
     }
 }
 ?>
-    <?php
-    $title = "Connexion";
-    require_once "includes/header.php"; ?>
-    <h2>Connexion</h2>
-    <form action="" method="post">
-    <input type="email" name="email" id="email" placeholder="Adresse de messagerie" required>
-    <input type="password" name="pass" id="pass" placeholder="Mot de passe" required>
-    <button type="submit" class="btn-confirm">Me connecter</button>
-    </form>
+<?php
+$title = "Connexion";
+require_once "includes/header.php"; ?>
+<h2>Connexion</h2>
+<form action="" method="post">
+    <div>
+        <label for="email">Identifiants</label>
+        <input type="email" name="email" id="email" placeholder="Adresse de messagerie" required>
+    </div>
+    <div>
+        <label for="email">Mot de passe</label>
+        <input type="password" name="pass" id="pass" placeholder="Mot de passe" required>
+    </div>
+    <div class="btn">
+        <button type="submit" class="btn-confirm">Se connecter</button>
+    </div>
+</form>
 
-    <?php require_once "includes/footer.php"; ?>
+<?php require_once "includes/footer.php"; ?>
