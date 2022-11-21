1b38e360fe483984ce5715873bb1f87f3550a729
Hugues Touyon <hugues.touyon@gmail.com>
2022-11-21 12:14:59 +0100

    v0.1

---
+++ connexion.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+if (!isset($_SESSION["user"])) {
+    // header("Location: connexion.php");
+    // exit;
+}
+
+?>
+
+<?php
+$title = "Accueil";
+require_once "includes/header.php"; ?>
+
+<form action="" method="post">
+<input type="email" name="email" id="email" placeholder="Adresse de messagerie" required>
+<input type="password" name="pass" id="pass" placeholder="Mot de passe" required>
+<button type="submit" class="btn-confirm">Me connecter</button>
+</form>
+
+<?php require_once "includes/footer.php"; ?>
---
+++ includes/connect.php
@@ -0,0 +1,14 @@
+<?php
+    $dbname = "touristgame";
+    $dbhost = "localhost";
+    $dbpass = "root";
+    $dbuser = "root";
+    try {
+        $dsn = "mysql:dbname=".$dbname.";host=".$dbhost;
+        $db = new PDO($dsn, $dbuser, $dbpass);
+        $db->exec("SET NAMES utf8");
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    }catch(PDOException $e) {
+        die($e->getMessage());
+    }
+?>
